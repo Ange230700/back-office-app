2bb587099f1471116f4a0be6df88676e92f9e235
Ange230700 <kouakouangeericstephane@gmail.com>
2025-02-26 12:58:31 +0100

    refactorization: setting up stuff

--- src/models/collection/manager.php
+++ src/models/collection/manager.php
@@ -53,22 +53,6 @@ function addCollectedWastesInformation(PDO $pdo, $wasteTypesSubmitted, $quantiti
         }
     }
 }
-
-function addVolunteersToCollection(PDO $pdo, $volunteersAssigned, $collectionId)
-{
-    try {
-        $sqlQueryToInsertVolunteersIntoCollection = "INSERT INTO benevoles_collectes (id_collecte,      id_benevole) VALUES (?, ?)";
-        $statementToAddVolunteersToCollection = $pdo->prepare($sqlQueryToInsertVolunteersIntoCollection);
-        foreach ($volunteersAssigned as $volunteerId) {
-            if (!$statementToAddVolunteersToCollection->execute([$collectionId, $volunteerId])) {
-                die("Erreur lors de la mise à jour des bénévoles.");
-            }
-        }
-    } catch (PDOException $e) {
-        echo "Erreur de base de données : " . $e->getMessage();
-        exit;
-    }
-}
 /* ============================================== */
 
 /* ------------------- READ ------------------- */
@@ -244,17 +228,4 @@ function updateCollectedWasteDetails(PDO $pdo, $collectionId, $wasteTypesSubmitt
 /* ============================================== */
 
 /* ------------------- DELETE ------------------- */
-function deleteVolunteersFromCollection(PDO $pdo, $collectionId)
-{
-    try {
-        $sqlQueryToDeleteVolunteersFromCollection = "DELETE FROM benevoles_collectes WHERE  id_collecte = ?";
-        $statementToDeleteVolunteersFromCollection = $pdo->prepare($sqlQueryToDeleteVolunteersFromCollection);
-        if (!$statementToDeleteVolunteersFromCollection->execute([$collectionId])) {
-            die("Erreur lors de la suppression des bénévoles.");
-        }
-    } catch (PDOException $e) {
-        echo "Erreur de base de données : " . $e->getMessage();
-        exit;
-    }
-}
 /* ============================================== */
